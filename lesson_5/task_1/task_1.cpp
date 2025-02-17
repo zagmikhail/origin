842a3057f424b6ea6d7616c8e1c2c41d34ff62af
Mikhail <zagmikhail@gmail.com>
2025-02-17 23:47:34 +0500

    task5_1 added

---
+++ lesson_5/task_1/task_1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+class Figure {
+protected:
+	int side;
+public:
+	Figure() {
+		side = 0;
+	}
+
+	int get_side() {
+		return side;
+	}
+};
+
+class Triangle: public Figure {
+public:
+	Triangle() {
+		side = 3;
+	}
+	
+};
+
+class Quadrangle: public Figure {
+public:
+	Quadrangle() {
+		side = 4;
+	}
+};
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	Figure figure;
+	Triangle triangle;
+	Quadrangle quauadrangle;
+	std::cout << "Количество сторон:" << std::endl;
+	std::cout << "Фигура: " << figure.get_side() << std::endl;
+	std::cout << "Треугольник: " << triangle.get_side() << std::endl;
+	std::cout << "Четырёхугольник: " << quauadrangle.get_side() << std::endl;
+
+}
+
+
+
