b86e31e78ee5911f3005a47e5ff72f1374cbae8d
Mikhail <zagmikhail@gmail.com>
2025-03-15 14:51:49 +0500

    task_7_1 and task_7_2 added

---
+++ lesson_7/task_1/task_1.cpp
@@ -0,0 +1,36 @@
+﻿#define MODE 1
+#include <iostream>
+#include <Windows.h>
+
+double func_add(const double& num_1, const double& num_2) {
+	return num_1 + num_2;
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+#ifndef MODE
+	std::cout << "Макрос MODE не определён! Задайте значение." << std::endl;
+#elif MODE == 0
+	std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE == 1
+	std::cout << "Работаю в боевом режиме!" << std::endl;
+
+	double user_num_1 = 0;
+	double user_num_2 = 0;
+
+	std::cout << "Введите первое число: ";
+	std::cin >> user_num_1;
+	std::cout << "Введите второе число: ";
+	std::cin >> user_num_2;
+	std::cout << "Сумма равна: " << func_add(user_num_1, user_num_2) << std::endl;
+
+#else
+	std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+#endif 
+
+return 0;
+   
+}
---
+++ lesson_7/task_2/task_2.cpp
@@ -0,0 +1,15 @@
+﻿#define SUB(a, b) ((a) - (b))
+#include <iostream>
+
+int main()
+{
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+
+	return 0;
+}
+
